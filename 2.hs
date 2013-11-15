441946452ed44d76339e7352936c4bafe4315550
Chris Wendt <chrismwendt@gmail.com>
2013-11-15 16:09:41 -0500

    Replace unused parameters with wildcards

--- 2.hs
+++ 2.hs
@@ -1,6 +1,6 @@
 -- Find the last but one element of a list.
 
 secondToLast [] = error "secondToLast of empty list"
-secondToLast [x] = error "secondToLast of list of length 1"
-secondToLast [a,b] = a
-secondToLast (x:xs) = secondToLast xs
+secondToLast [_] = error "secondToLast of list of length 1"
+secondToLast [a,_] = a
+secondToLast (_:xs) = secondToLast xs
