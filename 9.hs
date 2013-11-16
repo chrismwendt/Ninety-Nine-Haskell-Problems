e466b4d2a74894e8a3bd78ec76a16ffd2bdf07f3
Chris Wendt <chrismwendt@gmail.com>
2013-11-15 23:01:43 -0500

    Fix output of pack
    pack "hello"
    [["h"],["e"],["ll"],["o"]] -- incorrect
    ["h","e","ll","o"] -- correct

--- 9.hs
+++ 9.hs
@@ -1,5 +1,5 @@
 -- Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
 
 pack [] = []
-pack (x:xs) = [left] : pack right
+pack (x:xs) = left : pack right
     where (left,right) = span (== x) (x:xs)
