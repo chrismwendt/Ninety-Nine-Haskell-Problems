97c3b31e8adad6a375fd1645c2ccae4f404e6abd
Chris Wendt <chrismwendt@gmail.com>
2013-11-15 19:41:24 -0500

    Add an alternative solution to Problem 8 using foldr

--- 8.hs
+++ 8.hs
@@ -5,3 +5,8 @@ compress [x] = [x]
 compress (a:b:xs)
     | b == a = compress (a:xs)
     | otherwise = a : compress (b:xs)
+
+compress' l = foldr compressor [] l
+    where compressor a b
+            | null b || head b /= a = a : b
+            | otherwise = b
