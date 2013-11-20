fdaf895cf7a23f4ca9682f03f6c8ca1770e7e387
Chris Wendt <chrismwendt@gmail.com>
2013-11-19 23:59:49 -0500

    Replace splitting and recursion with zip, cycle, and a list comprehension in Problem 16

--- 16.hs
+++ 16.hs
@@ -1,5 +1,3 @@
 -- Drop every N'th element from a list.
 
-dropEvery [] _ = []
-dropEvery l n = let (section, rest) = splitAt (n - 1) l
-                in section ++ dropEvery (if null rest then [] else tail rest) n
+dropEvery l n = [ i | (i, j) <- zip l $ cycle [1..n], j /= n]
