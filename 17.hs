069a8d65c3ca13c8f0ca51277c0bf85d5e48e964
Chris Wendt <chrismwendt@gmail.com>
2013-11-21 00:24:03 -0500

    Solve Problem 17

---
+++ 17.hs
@@ -0,0 +1,9 @@
+-- Split a list into two parts; the length of the first part is given.
+
+split [] n
+    | n < 0 = error "negative index"
+    | otherwise = error "index too large"
+split xss@(x:xs) n
+    | n == 0 = ([], xss)
+    | otherwise = (x : left, right)
+    where (left, right) = split xs (n - 1)
