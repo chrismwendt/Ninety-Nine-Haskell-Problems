6cb88fbae49673fcf554dc42032001a64dc77be6
Chris Wendt <chrismwendt@gmail.com>
2013-11-21 14:51:54 -0500

    Remove redundant parentheses

--- 19.hs
+++ 19.hs
@@ -1,4 +1,4 @@
 -- Rotate a list N places to the left.
 
 rotate l n = right ++ left
-    where (left, right) = splitAt (n `mod` (length l)) l
+    where (left, right) = splitAt (n `mod` length l) l
